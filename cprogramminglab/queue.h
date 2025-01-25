cb454c1eaa829c2f4b63c5eeaf36a988a69f9b78
Raltour <15610159165lmz@gmail.com>
2025-01-25 21:39:21 +0800

    在头文件中添加了要实现的函数以及注释

--- cprogramminglab/queue.h
+++ cprogramminglab/queue.h
@@ -8,3 +8,23 @@ typedef struct list_ele {
 typedef struct {
     list_ele_t *head; /* First element in the queue */
 } queue_t;
+
+// Create a new, empty queue.
+queue_t *queue_new();
+//Free all storage used by a queue.
+void queue_free(queue_t *queue);
+//Attempt to insert a new element at the head of the queue (LIFO discipline).
+void queue_insert_head(queue_t *queue, char *value);
+//Attempt to insert a new element at the tail of the queue (FIFO discipline).
+void queue_insert_tail(queue_t *queue, char *value);
+//Attempt to remove the element at the head of the queue.
+char *queue_remove_head(queue_t *queue);
+//Compute the number of elements in the queue.
+int queue_size(queue_t *queue);
+/*
+* Reorder the list so that the queue elements are reversed in order.
+* This function should not allocate or free any list elements
+* (either directly or via calls to other functions that allocate or free list elements.)
+* Instead, it should rearrange the existing elements.
+ */
+void queue_reverse(queue_t *queue);
