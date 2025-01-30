053a71e83ff1e0bdd0fe8f9baed94a89a6e6e284
Raltour <15610159165lmz@gmail.com>
2025-01-30 10:51:41 +0800

    完成了第一个函数，但是文件不能正常编译、检查，打算迁移到linux环境进行后续编程

--- datalab/bits.c
+++ datalab/bits.c
@@ -1,7 +1,7 @@
 /* 
  * CS:APP Data Lab 
  * 
- * <Please put your name and userid here>
+ * Raltour
  * 
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
@@ -143,7 +143,7 @@ NOTES:
  *   Rating: 1
  */
 int bitXor(int x, int y) {
-  return 2;
+  return (~x & y) + (~y & x);
 }
 /* 
  * tmin - return minimum two's complement integer 
