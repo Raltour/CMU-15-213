27eb2c1fceaf36f7bb8b0bda90d0c05e9328ed75
Raltour <15610159165lmz@gmail.com>
2025-03-04 16:16:10 +0800

    完善了Cache结构的代码，接下来编写对于结构体的各种函数操作

--- cachelab/csim.c
+++ cachelab/csim.c
@@ -32,15 +32,46 @@ void deleteline(Line* line) {
 }
 
 typedef struct Cache {
-
+    int max_lines;
+    int sets_num;
+    Line*** sets;
 }Cache;
+
+Cache* newCache(int S, int E, int tag_size, int block_size) {
+    
+    Line*** sets = (Line***)malloc(sizeof(Line**) * S);
+    for (int i = 0; i < S; i++) {
+        sets[i] = (Line**)malloc(sizeof(Line*) * E);
+        for (int j = 0; j < E; j++) {
+            sets[i][j] = newline(tag_size, block_size);
+        }
+    }
+
+    Cache cache = {
+        .max_lines = E,
+        .sets_num = S,
+        .sets = sets
+    };
+
+    return &cache;
+}
+
+void deletecache(Cache* cache) {
+    for (int i = 0; i < cache->sets_num; i++) {
+        for (int j = 0; j < cache->max_lines; j ++) {
+            deleteline(cache->sets[i][j]);
+        }
+        free(cache->sets[i]);
+    }
+    free(cache->sets);
+}
 
 
 int main(int argc, int* argv)
 {
     int opt, s, E, b;
 
-    while ((opt = getopt(argc, argv, "sEb:")) != -1) {
+    while ((opt = getopt(argc, argv, "s:E:b:")) != -1) {
         switch(opt) {
             case 's' :
                 s = atoi(optarg);
